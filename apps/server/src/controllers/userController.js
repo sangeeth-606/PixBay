b4e71ea9fb131085e3fe53b127a10034349c4675
sangeeth-606 <sangeeth999123@gmail.com>
2025-04-03 23:26:52 +0530

    feat(server): add updateUser function and route for updating user information

--- apps/server/src/controllers/userController.js
+++ apps/server/src/controllers/userController.js
@@ -18,6 +18,17 @@ const addUser = (req, res) => {
         res.status(500).send(error.message);
     }
 }
+const updateUser = (req, res) => {
+    try {
+        const { email } = req.params;
+        const user = req.body;
+        const userIndex = users.findIndex(user => user.email === email);
+        users[userIndex] = { ...users[userIndex], ...user };
+        res.status(200).json({ message: "user updated successfully", user: users[userIndex] });
+    }
+    catch (error) {
+        res.status(500).send(error.message);
+    }
+}
 
-
-export { getUsers,addUser };
+export { getUsers,addUser,updateUser };

d040f91ec2b0ab15cde8dd9eb4f3ad11ca628363
sangeeth-606 <sangeeth999123@gmail.com>
2025-04-06 16:37:04 +0530

    feat(client): add Dashboard component and enhance LandingPage with user name handling

--- apps/server/src/routes/workspaceRoutes.js
+++ apps/server/src/routes/workspaceRoutes.js
@@ -1,6 +1,6 @@
 import express from 'express'
-import { createWorkspace,joinWorkspace,getUserWorkspaces } from '../controllers/workspaceController';
-import authMiddleware from '../middleware/authMiddleware';
+import { createWorkspace,joinWorkspace,getUserWorkspaces } from '../controllers/workspaceController.js';
+import authMiddleware from '../middleware/authMiddleware.js';
 const router= express.Router();
 
 router.post('/create', authMiddleware ,createWorkspace);       

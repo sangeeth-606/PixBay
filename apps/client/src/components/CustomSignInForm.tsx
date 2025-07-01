6120ae1003830da736363d887eb181cbd9fc62f2
sangeeth-606 <sangeeth999123@gmail.com>
2025-07-01 16:40:36 +0530

    fix: Remove duplicate import of useNavigate in CustomSignInForm and adjust import for EventClickArg in Calendar

--- apps/client/src/components/CustomSignInForm.tsx
+++ apps/client/src/components/CustomSignInForm.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
-import { useNavigate } from "react-router-dom";
 
 interface CustomSignInFormProps {
   hideHeading?: boolean;
